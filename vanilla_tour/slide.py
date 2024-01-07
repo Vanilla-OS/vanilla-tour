ca6b21291bc39c51d885eca5716f00772229f8e5
Muqtxdir <muqtxdir@gmail.com>
2024-01-08 00:34:21 +0530

    fix: Support actions in tour #5:
    -  refactor slide to make use of `AdwSttausPage` and hidden `GtkButton`
    - fix gresource for png asset

--- vanilla_tour/slide.py
+++ vanilla_tour/slide.py
@@ -17,23 +17,36 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Adw, Gtk
-
+from gi.repository import Adw, Gtk, GLib
+import webbrowser
 
 @Gtk.Template(resource_path="/org/vanillaos/Tour/blp/slide.ui")
 class Slide(Adw.Bin):
     __gtype_name__ = "Slide"
 
-    title = Gtk.Template.Child()
-    description = Gtk.Template.Child()
+    status_page = Gtk.Template.Child()
     assets_svg = Gtk.Template.Child()
+    btn_action = Gtk.Template.Child()
 
     def __init__(self, slide, **kwargs):
         super().__init__(**kwargs)
         self.__slide = slide
         self.__build_ui()
 
+        self.btn_action.connect("clicked", self.__on_btn_action_clicked)
+
     def __build_ui(self):
         self.assets_svg.set_resource(self.__slide["resource"])
-        self.title.set_label(self.__slide["title"])
-        self.description.set_label(self.__slide["description"])
+        self.status_page.set_title(self.__slide["title"])
+        self.status_page.set_description(self.__slide["description"])
+
+        if "action" in self.__slide:
+            self.btn_action.set_visible(True)
+            self.btn_action.set_label(self.__slide["action"]["title"])
+
+    def __on_btn_action_clicked(self, widget):
+        if "url://" in self.__slide["action"]["callback"]:
+                webbrowser.open(self.__slide["action"]["callback"].split("url://")[1])
+
+        elif "cmd://" in self.__slide["action"]["callback"]:
+            GLib.spawn_command_line_async(self.__slide["action"]["callback"].split("cmd://")[1])   
